0417f7d9e611b0e25db3e4f42895fccf959107f6
Samed Karakus <karakusnavy@gmail.com>
2019-10-24 17:47:05 +0300

    Update App.js

--- App.js
+++ App.js
@@ -22,14 +22,14 @@ var options = {
   },
 };
 var firebaseConfig = {
-  apiKey: "AIzaSyB6nFbNVQIci90Q9dI7hN4ZsKPaDF4y6ec",
-  authDomain: "reactnativetest-1f0da.firebaseapp.com",
-  databaseURL: "https://reactnativetest-1f0da.firebaseio.com",
-  projectId: "reactnativetest-1f0da",
-  storageBucket: "reactnativetest-1f0da.appspot.com",
-  messagingSenderId: "90332168774",
-  appId: "1:90332168774:web:fb075d90d46d48cfa1c047",
-  measurementId: "G-K9029NBRQX"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 firebase.initializeApp(firebaseConfig);
 export default class App extends React.Component {
